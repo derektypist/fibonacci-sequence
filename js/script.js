af577f345177d1c8a0b8666f31c5b11e55fdd4a4
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-01-24 13:51:13 +0000

    Modified if statement on getNumberInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ function getNumberInfo() {
     // Get the Value of the Input Field
     let num = document.getElementById('mynumber').value;
     // Check if number is valid
-    if (isNaN(num) || num.toString().length == 0 || num<0 || num>5000 || !Number.isInteger(Number(num))) {
-        txt += `Invalid input.  Please enter a whole number between 0 and 5000.`;
+    if (isNaN(num) || num.toString().length == 0 || num<0 || num>1000 || !Number.isInteger(Number(num))) {
+        txt += `Invalid input.  Please enter a whole number between 0 and 1000.`;
     } else {
         txt += `You have entered the number ${num}. <p>`;
-        txt += `fibonacci(${num}) returns ${fibonacci(num)}`;
+        txt += `fibonacci(${num}) returns ${fibonacci(num)}.`;
     }
 
     // Display Information in the Browser
